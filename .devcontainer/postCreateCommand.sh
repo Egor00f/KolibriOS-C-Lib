94df796554f464f73b0e4399bc88233970f525e6
Егор <148970641+Egor00f@users.noreply.github.com>
2024-11-23 13:06:10 +0000

    update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,11 @@
-apt update 
-apt install fasm p7zip-full wget cmake
+git submodules update --remote --init
 
-git clone https://github.com/Egor00f/kolibrios-gcc-toolchain.git 
-kolibrios-gcc-toolchain/./install
+sudo curl https://git.kolibrios.org/api/packages/Egor00f/debian/repository.key -o /etc/apt/keyrings/gitea-Egor00f.asc
+echo "deb [signed-by=/etc/apt/keyrings/gitea-Egor00f.asc] https://git.kolibrios.org/api/packages/Egor00f/debian bookworm main" | sudo tee -a /etc/apt/sources.list.d/gitea.list
+
+sudo apt update 
+echo '/home/toolchain' | sudo apt install kos32-gcc -y
+
+
 cd .. 
 git clone https://git.kolibrios.org/KolibriOS/kolibrios.git
